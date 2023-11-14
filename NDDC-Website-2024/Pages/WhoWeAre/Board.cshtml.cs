719028f5ecf1458532f5d0dbaf7b840612936579
Orchiid <joannatolofari@gmail.com>
2023-11-14 15:30:56 +0100

    New Commit

---
+++ NDDC-Website-2024/Pages/WhoWeAre/Board.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.WhoWeAre
+{
+    public class BoardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
