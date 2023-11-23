9e375efa7652f935be91ae8408507af3a9111b3f
Orchiid <joannatolofari@gmail.com>
2023-11-23 19:02:12 +0100

    new commit

---
+++ NDDC-Website-2024/Pages/WhatWeDo/Projects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.WhatWeDo
+{
+    public class ProjectsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
