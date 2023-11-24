e0466fc72400d7984cc4ae95c9dc9ed0e9031fd3
Orchiid <joannatolofari@gmail.com>
2023-11-24 14:46:25 +0100

    new

---
+++ NDDC-Website-2024/Pages/Services/OnlineForms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.Services
+{
+    public class OnlineFormsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NDDC-Website-2024/Pages/Services/Tenders.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.Services
+{
+    public class TendersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
