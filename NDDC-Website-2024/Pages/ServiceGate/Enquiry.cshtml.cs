a04116bf83f4568e645e0920886873d3f98f821f
Orchiid <joannatolofari@gmail.com>
2024-01-29 14:05:52 +0100

    commit

---
+++ NDDC-Website-2024/Pages/ServiceGate/Enquiry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.ServiceGate
+{
+    public class EnquiryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
