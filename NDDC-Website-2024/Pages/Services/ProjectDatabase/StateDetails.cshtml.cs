9118ab0410522d7d2fd09b4ca4953ef415ac5db8
Orchiid <joannatolofari@gmail.com>
2024-01-31 16:00:27 +0100

    new

---
+++ NDDC-Website-2024/Pages/Services/ProjectDatabase/StateDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.Services.ProjectDatabase
+{
+    public class StateDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
