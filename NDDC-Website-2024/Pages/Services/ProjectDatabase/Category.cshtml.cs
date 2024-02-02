52162db489f3ba75a028137db36b3d26592e9274
Orchiid <joannatolofari@gmail.com>
2024-02-02 12:32:54 +0100

    new

---
+++ NDDC-Website-2024/Pages/Services/ProjectDatabase/Category.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.Services.ProjectDatabase
+{
+    public class CategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
