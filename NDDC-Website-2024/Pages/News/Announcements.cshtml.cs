f48092308fd8d8b7a369831ee32889b92d923667
Orchiid <joannatolofari@gmail.com>
2024-02-12 15:03:28 +0100

    push

---
+++ NDDC-Website-2024/Pages/News/Announcements.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.News
+{
+    public class AnnouncementsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
