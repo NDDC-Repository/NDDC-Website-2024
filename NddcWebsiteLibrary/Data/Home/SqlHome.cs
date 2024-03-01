7d99c34855bae792bd68dc56120710b1fb11d81f
Orchiid <joannatolofari@gmail.com>
2024-03-01 15:17:37 +0100

    Rounding up website

--- NddcWebsiteLibrary/Data/Home/SqlHome.cs
+++ NddcWebsiteLibrary/Data/Home/SqlHome.cs
@@ -94,11 +94,11 @@ namespace NddcWebsiteLibrary.Data.Home
         //Tenders
         public List<MyTenderModel> ViewAllTenders()
         {
-            return db.LoadData<MyTenderModel, dynamic>("Select Id, Titel, Category, DocumentUrl, AdvertDate, DeadlioneDate From Tenders Order By Id DESC", new { }, connectionStringName, false).ToList();
+            return db.LoadData<MyTenderModel, dynamic>("Select Id, Title, Category, DocumentUrl, AdvertDate, DeadlineDate From Tenders Order By Id DESC", new { }, connectionStringName, false).ToList();
         }
         public MyTenderModel ViewTenderDetails(int id)
         {
-            return db.LoadData<MyTenderModel, dynamic>("Select Id, Titel, Category, DocumentUrl, AdvertDate, DeadlioneDate From Tenders Where Id = @Id Order By Id DESC", new { Id = id }, connectionStringName, false).FirstOrDefault();
+            return db.LoadData<MyTenderModel, dynamic>("Select Id, Title, Category, DocumentUrl, AdvertDate, DeadlineDate From Tenders Where Id = @Id Order By Id DESC", new { Id = id }, connectionStringName, false).FirstOrDefault();
         }
 
         //Breaking News
