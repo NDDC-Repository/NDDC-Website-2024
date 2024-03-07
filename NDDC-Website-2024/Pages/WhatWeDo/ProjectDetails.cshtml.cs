589d6349b1e5ce6c3c77c53d540c87562b89783d
Orchiid <joannatolofari@gmail.com>
2024-03-07 17:12:35 +0100

    Added Success page for i reports and text for project details

---
+++ NDDC-Website-2024/Pages/WhatWeDo/ProjectDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NDDC_Website_2024.Pages.WhatWeDo
+{
+    public class ProjectDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
